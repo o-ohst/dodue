eaadad20cdb91e6386e60ac6642e174949cbdd18
Hao Sitong <hst1142007403@gmail.com>
2022-01-20 02:38:48 +0800

    cookies config

--- main.go
+++ main.go
@@ -241,6 +241,9 @@ func checkUserCookie(next http.Handler) http.Handler {
 func cors(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		log.Print(r)
+
 		w.Header().Set("Access-Control-Allow-Origin", "https://dodue.netlify.app/")
     	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
     	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, token, user_id, api_key, task_id, category_id, username, password")
